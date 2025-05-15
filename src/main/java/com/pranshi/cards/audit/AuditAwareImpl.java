327e8ee71e17673508b2cef9c82f28bfa1a87e51
Vipin <=vipinvats42@gmail.com>
2025-05-15 15:10:11 +0530

    first commit

---
+++ src/main/java/com/pranshi/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,17 @@
+package com.pranshi.cards.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        return Optional.of("CARDS_MS");
+    }
+
+}
